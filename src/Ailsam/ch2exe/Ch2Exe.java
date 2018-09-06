81bfb97ed408ad7cdf5c4eb42de303c0c6391e35
ailsa.mensinger <ailsa.mensinger@CLC-LAB209-03.TCSD.tooelesd.org>
2018-09-06 14:09:59 -0600

    Chapter 2 examples

---
+++ src/Ailsam/ch2exe/Ch2Exe.java
@@ -0,0 +1,18 @@
+package Ailsam.ch2exe;
+/*
+ * Ailsa Mensinger
+ * 9-6-18
+ * ch2 exe
+ */
+
+public class Ch2Exe {
+
+	public static void main(String[] args) {
+		// display some text
+		System.out.println("Welcome to Java");
+		System.out.println((25*.75)+9); 
+		
+
+	}
+
+}
